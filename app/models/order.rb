9fbcf197413c9aa491419b2742724be1b6396276
selmenm <selmen.mhamdi@live.fr>
2019-03-04 19:36:00 +0100

    suppression du mailer

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
 	has_many :carts
 
-  after_create :confirm_cart
-  def confirm_cart # TO BUILD MAILER + VIEW !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    OrderMailer.confirm_cart(self).deliver_now
-  end
+#after_create :confirm_cart
+#def confirm_cart # TO BUILD MAILER + VIEW !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#  OrderMailer.confirm_cart(self).deliver_now
+#end
 end
