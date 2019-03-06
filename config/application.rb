b732638c57ad8075c02c5d3e154204bc538c97ab
selmenm <selmen.mhamdi@live.fr>
2019-03-06 10:51:45 +0100

    modification config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,10 @@ module TheKittens
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
 
-config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
+
+  end
 
 end
 
